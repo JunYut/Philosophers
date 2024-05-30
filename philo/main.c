9f2509f92573c0e405f443c3d477868706a6d001
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-05-30 10:36:56 +0800

    philo/src/init.c: fixed invalid write size of 'memset()' in 'init_env()'

--- philo/main.c
+++ philo/main.c
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 14:33:52 by tjun-yu           #+#    #+#             */
-/*   Updated: 2024/05/30 10:26:22 by we               ###   ########.fr       */
+/*   Updated: 2024/05/30 10:35:03 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,4 +21,5 @@ int	main(int argc, char *argv[])
 
 	init_env(&table, 5);
 	log_activity(table.start_time, 1, "is eating");
+	clean_up(&table);
 }
--- philo/src/init.c
+++ philo/src/init.c
@@ -6,7 +6,7 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/29 17:02:39 by we                #+#    #+#             */
-/*   Updated: 2024/05/30 10:31:12 by we               ###   ########.fr       */
+/*   Updated: 2024/05/30 10:35:43 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ void	init_env(t_table *table, int philo_count)
 	table->fork = (char *)ft_malloc(philo_count);
 	table->start_time = get_time_ms();
 	table->philo_count = philo_count;
-	memset(table->fork, 1, sizeof(int) * philo_count);
+	memset(table->fork, 1, philo_count);
 }
 
 void	clean_up(t_table *table)
