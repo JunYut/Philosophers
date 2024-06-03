6eac6c1ba3e80b559653174e7fbcf8cf5be843d0
Tan Jun Yu <tanjunyu8888@gmail.com>
2024-06-03 11:59:49 +0800

    philo/src/debug.c: implemented 'print_forks()' to monitor state of forks

--- philo/src/debug.c
+++ philo/src/debug.c
@@ -6,11 +6,22 @@
 /*   By: we <we@student.42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/31 17:03:07 by we                #+#    #+#             */
-/*   Updated: 2024/05/31 17:03:26 by we               ###   ########.fr       */
+/*   Updated: 2024/06/03 11:56:35 by we               ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+void	print_forks(char *forks, int count)
+{
+	int	i;
+
+	i = -1;
+	while (++i < count)
+		printf("forks[%d]: %d\n", i, forks[i]);
+	printf("\n");
+
+}
 
 void	print_id(t_philo *philos, int count)
 {
